1b1493cc050fe01e14621c4dee03c4845f17d509
slimissa <slimissabara@gmail.com>
2026-08-13 12:37:16 +0100

    Remove Rust build artifacts from git; add target/ to .gitignore

--- wrappers/rust/target/debug/deps/cfg_if-72a7719c5a503f05.d
+++
@@ -1,7 +0,0 @@
-/home/slim/Documents/Exchange calendar registry/exchange-calendar/wrappers/rust/target/debug/deps/cfg_if-72a7719c5a503f05.d: /home/slim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/slim/Documents/Exchange calendar registry/exchange-calendar/wrappers/rust/target/debug/deps/libcfg_if-72a7719c5a503f05.rlib: /home/slim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/slim/Documents/Exchange calendar registry/exchange-calendar/wrappers/rust/target/debug/deps/libcfg_if-72a7719c5a503f05.rmeta: /home/slim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/slim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs:
